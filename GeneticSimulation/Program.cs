ed457ca720c804693253c57b3a55b538ac214e6f
Mike <zmish93@ya.ru>
2015-09-09 00:59:40 +0300

    Init

---
+++ GeneticSimulation/Program.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GeneticSimulation
+{
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        private static void Main()
+        {
+
+        }
+    }
+}
